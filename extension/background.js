a047c68bf8f9938b6422cb46422b5f9325ea12d9
Mathias Ducancel <mathiasduc@gmail.com>
2016-12-01 11:05:34 +0100

    Add switch type checkboxes and page resizing

--- extension/background.js
+++ extension/background.js
@@ -11,7 +11,7 @@ var app = {
 	},
 	listeners: function(){
 		var me = this;
-		$("#button_submit").on("click", me.form_values.bind(me));
+		$("#button_submit").on("click", me.formValidation.bind(me));
 	},
 
 	formSettings:function(){
@@ -22,17 +22,52 @@ var app = {
 	form_values:function(){
 		var form_is_valid = this.formSelector.form('is valid');
 		if(form_is_valid){
-			var values = this.formSelector.form('get values')
-			console.log(values);
+			var values = this.formSelector.form('get values');
 			return(values);
 		}else{
 			return(false);
 		}
 	},
 
+	formStringify: function(){
+		var values = this.form_values();
+		console.log(values);
+		/*if(values){
+			switch(values){
+			
+			}
+			
+		}else{
+			return(false);
+		}*/
+	},
+	
+	formValidation:function(){
+		var stringValues = this.formStringify();
+		if(stringValues){
+			
+		}
+	},
+
 	checkboxes:function(){
+		var canceled_after_48h = $('#canceled_after_48h');
+		var canceled_before_48h = $('#canceled_before_48h');
 		$('.checkbox').checkbox();
-		//il faut rajouter les  regles semantic
+		
+		/*Ces deux fonctions permetent de s'assurer que une seul checkbox peut etre check.
+		Ces deux fonctions tiennent compte du niveau d'absatraction que semantic implemente.
+		On remarque que on cible la div de la checkbox et pas la checkbox elle meme.
+		Ces deux "event" sont, si j'ain bien compris, des raccourcis pour des listeners.
+		L'on pourrais aussi utiliser cette fonction listener pour chaqu'une des deux checkbox: */
+		
+		/* canceled_after_48h.on('change',function(){
+			if(canceled_before_48h.checkbox('is checked')){
+				canceled_before_48h.checkbox('uncheck')
+			}
+		});*/
+
+		$('#canceled_after_48h').checkbox('attach events', '#canceled_before_48h', 'uncheck');
+		$('#canceled_before_48h').checkbox('attach events', '#canceled_after_48h', 'uncheck');
 	},
 };
 $(document).ready(function(){
